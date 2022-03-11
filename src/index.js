7142add88fcd5ab8bd55d07ff5fbe1e4c2ace5df
Haja Asmath <asmath1997@gmail.com>
2022-03-11 22:32:53 +0530

    reformat index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,46 @@
 import homePage from './pages/home-page.js';
 import menuPage from './pages/menu-page.js';
 import contactPage from './pages/contact-page.js';
-import navTab from './pages/nav-tab.js'
-import './style/style.css'
+import navTab from './pages/nav-tab.js';
+import './style/style.css';
 import domUtils from './utils/domUtils.js';
 
-(function() {
-    let mainContent;
+(function () {
+  let mainContent;
 
-    function init() {
-        let content = domUtils.createDiv('content');
+  function init() {
+    const content = domUtils.createDiv('content');
 
-        content.appendChild(navTab());
-        mainContent = document.createElement('div');
-        mainContent.setAttribute('id','mainContent');
-        mainContent.appendChild(homePage());
-        content.appendChild(mainContent);
+    content.appendChild(navTab());
+    mainContent = document.createElement('div');
+    mainContent.setAttribute('id', 'mainContent');
+    mainContent.appendChild(homePage());
+    content.appendChild(mainContent);
 
-        document.body.appendChild(content);
-        
-    }
+    document.body.appendChild(content);
+  }
 
-    function addEvents() {
-        document.querySelector('#home').addEventListener('click', displayHomePage);
-        document.querySelector('#menu').addEventListener('click', displayMenuPage);
-        document.querySelector('#contact').addEventListener('click', displayContactPage);
-    }
+  function addEvents() {
+    document.querySelector('#home').addEventListener('click', displayHomePage);
+    document.querySelector('#menu').addEventListener('click', displayMenuPage);
+    document.querySelector('#contact').addEventListener('click', displayContactPage);
+  }
 
-    function displayHomePage() {
-        mainContent.textContent = '';
-        mainContent.appendChild(homePage());
-    }
+  function displayHomePage() {
+    mainContent.textContent = '';
+    mainContent.appendChild(homePage());
+  }
 
-    function displayMenuPage() {
-        mainContent.textContent = '';
-        mainContent.appendChild(menuPage());
-    }
+  function displayMenuPage() {
+    mainContent.textContent = '';
+    mainContent.appendChild(menuPage());
+  }
 
-    function displayContactPage() {
-        mainContent.textContent = '';
-        mainContent.appendChild(contactPage());
-    }
+  function displayContactPage() {
+    mainContent.textContent = '';
+    mainContent.appendChild(contactPage());
+  }
 
-    init();
-    addEvents();
+  init();
+  addEvents();
 }());
--- src/utils/domUtils.js
+++ src/utils/domUtils.js
@@ -1,39 +1,39 @@
-let domUtils = (function() {
-    let createDiv = (id, className) => {
-        let div = document.createElement('div');
-        addClassAndId(div, className, id);
-        return div;
-    }
+const domUtils = (function () {
+  const addClassAndId = (element, className, id) => {
+    if (id != null && id !== undefined) element.setAttribute('id', id);
+    if (className != null && className !== undefined) element.classList.add(className);
+    return element;
+  };
 
-    let createPara = (id, className) => {
-        let p = document.createElement('p');
-        addClassAndId(p, className, id);
-        return p;
-    }
+  const createDiv = (id, className) => {
+    const div = document.createElement('div');
+    addClassAndId(div, className, id);
+    return div;
+  };
 
-    let createImg = (src, id, altText, className) => {
-        let img = document.createElement('img');
-        img.setAttribute('src', src);
-        img.setAttribute('alt', altText);
-        addClassAndId(img, className, id);
-        return img;
-    }
+  const createPara = (id, className) => {
+    const p = document.createElement('p');
+    addClassAndId(p, className, id);
+    return p;
+  };
 
-    let createLabel = (id, className) => {
-        let label = document.createElement('label');
-        addClassAndId(label, className, id);
-        return label;
-    }
+  const createImg = (src, id, altText, className) => {
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
+    img.setAttribute('alt', altText);
+    addClassAndId(img, className, id);
+    return img;
+  };
 
-    let addClassAndId = (element, className, id) => {
-        if (id != null && id != undefined)
-            element.setAttribute('id', id);
-        if (className != null && className != undefined) 
-            element.classList.add(className);
-        return element;
-    }
+  const createLabel = (id, className) => {
+    const label = document.createElement('label');
+    addClassAndId(label, className, id);
+    return label;
+  };
 
-    return {createDiv, createPara, createImg, createLabel};
+  return {
+    createDiv, createPara, createImg, createLabel,
+  };
 }());
 
 export default domUtils;
