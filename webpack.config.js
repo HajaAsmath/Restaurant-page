d25ce9c245bd316989b6cda48b4d2ac2c571e47c
Haja Asmath <asmath1997@gmail.com>
2022-03-13 23:18:10 +0530

    indendation problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: ['./src/index.js','./src/utils/domUtils.js','./src/pages/contact-page.js','./src/pages/home-page.js','./src/pages/menu-page.js',
-        './src/pages/nav-tab.js']
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Restaurant Page',
-        }),
+  mode: 'development',
+  entry: {
+    main: ['./src/index.js', './src/utils/domUtils.js', './src/pages/contact-page.js', './src/pages/home-page.js', './src/pages/menu-page.js',
+      './src/pages/nav-tab.js'],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, 
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-              },
-        ]
-    }
-}
+  },
+};
